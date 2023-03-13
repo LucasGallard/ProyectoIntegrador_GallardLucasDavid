f24f2bb1ea2997ecc969d0ebccf05a0502ebde98
LucasGallard <gallardlucas61@gmail.com>
2023-03-13 17:11:06 -0300

    hys2

--- ProyectoIntegrador_GallardLucasDavid/src/app/app.module.ts
+++ ProyectoIntegrador_GallardLucasDavid/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
+    HysComponent,
   ],
   imports: [
     BrowserModule,

568016adb4f0e65d9aa2840c8901d559a8f0f573
LucasGallard <gallardlucas61@gmail.com>
2023-04-12 21:33:59 -0300

    Creacion de Carpeta BackEnd

---
+++ ProyectoIntegrador_GallardLucasDavid/BackEnd/src/main/java/com/portfolio/ldg/LdgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ldg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdgApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_GallardLucasDavid/BackEnd/src/test/java/com/portfolio/ldg/LdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ldg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

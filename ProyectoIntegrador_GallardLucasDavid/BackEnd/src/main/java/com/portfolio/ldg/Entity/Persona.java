9be0ee6c9d754b61227bc2a688c98d9ad6c8b0ef
LucasGallard <gallardlucas61@gmail.com>
2023-04-23 15:39:25 -0300

    BE1

---
+++ ProyectoIntegrador_GallardLucasDavid/BackEnd/src/main/java/com/portfolio/ldg/Entity/Persona.java
@@ -0,0 +1,29 @@
+package com.portfolio.ldg.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String nombre;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String apellido;
+    
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String img;
+}
---
+++ ProyectoIntegrador_GallardLucasDavid/BackEnd/src/main/java/com/portfolio/ldg/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.ldg.Repository;
+
+import com.portfolio.ldg.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+    
+}
